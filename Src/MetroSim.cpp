674d8af3ff2faf5bd4d06f3512dd1a7d69cce7c5
brennanr7 <98706370+brennanr7@users.noreply.github.com>
2023-01-06 16:35:57 -0500

    Update MetroSim.cpp

--- Src/MetroSim.cpp
+++ Src/MetroSim.cpp
@@ -4,9 +4,9 @@
  * by Brennan Rivera, 02/22/2021
  * Stores passengers as they board the train, station, as well as running the
  * the simulation. Passengers are stored in a vector of queues, and a vector
- * of structs. This file communicates with MetroSim.h and PassengerQueue.h
- * order.
+ * of structs. This file uses MetroSim.h and PassengerQueue.h
  */
+
 #include "MetroSim.h"
 #include "PassengerQueue.h"
 
