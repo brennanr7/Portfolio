2d5d97eaa0f021cd628faea2a63d7b3f511e47a5
brennanr7 <98706370+brennanr7@users.noreply.github.com>
2023-01-06 15:20:02 -0500

    Update run.sh

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -1,4 +1,10 @@
 #bin/bash
-./build.sh $1"v"
 
+#$1 -> Exec
+#$2 -> NodeList
+#$3 -> Grid
+#$4 -> Source node
+#$5 -> Destination node
+#$6 -> Scrap folder path for outputted results
+#$7 -> Output file name
 ./$1"v1" $2 $3 $4 $5 $6 $7
