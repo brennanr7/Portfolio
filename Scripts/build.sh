70c06c1c9c434a650d703b56d85a1d79eda63f0a
brennanr7 <98706370+brennanr7@users.noreply.github.com>
2023-01-06 15:21:36 -0500

    Update build.sh

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,4 +1,4 @@
 #bin/bash
-g++ ../Programs/Version1/main.cpp ../Src/DataStorage.cpp -o $1"1"
 
-g++ ../Programs/Version2/main.cpp ../Src/DataStorage.cpp -o $1"2"
+#$1 -> Exec name
+g++ ../Programs/Version1/main.cpp ../Src/DataStorage.cpp -o $1
