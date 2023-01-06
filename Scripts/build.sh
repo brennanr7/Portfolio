acbb9d5bc398e53845c3c492a6e5f39ce49b3b26
brennanr7 <98706370+brennanr7@users.noreply.github.com>
2023-01-06 15:15:00 -0500

    Update build.sh

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,6 +1,4 @@
 #bin/bash
-g++ ../Programs/Version1/main.cpp ../Programs/Version1/DataStorage.cpp -o $1"1"
+g++ ../Programs/Version1/main.cpp ../Src/DataStorage.cpp -o $1"1"
 
-g++ ../Programs/Version2/main.cpp ../Programs/Version1/DataStorage.cpp -o $1"2"
-
-#g++ PRograms/Version3/main.cpp Programs/Version1/DataStorage.cpp -o $1"1"
+g++ ../Programs/Version2/main.cpp ../Src/DataStorage.cpp -o $1"2"
