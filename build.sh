644501c97e918b07092c8e03ca767092147f2b99
brennanr7 <98706370+brennanr7@users.noreply.github.com>
2023-01-06 15:13:46 -0500

    Delete build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-#bin/bash
-g++ ../Programs/Version1/main.cpp ../Programs/Version1/DataStorage.cpp -o $1"1"
-
-g++ ../Programs/Version2/main.cpp ../Programs/Version1/DataStorage.cpp -o $1"2"
-
-#g++ PRograms/Version3/main.cpp Programs/Version1/DataStorage.cpp -o $1"1"
